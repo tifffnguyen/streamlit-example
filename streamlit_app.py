dcf0c8a46a02e7a7bd05509f4714d67e1416399e
tifffnguyen <105865921+tifffnguyen@users.noreply.github.com>
2022-06-29 05:48:54 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,8 @@ import requests
 import json
 import pandas as pd
 
+api_key = "9bac03f9f1d1dc4b5cf06c47b0550cc36a71afe54e43330c8bc7f7802c126d70"
+
 filing_url = "https://www.sec.gov/ix?doc=/Archives/edgar/data/1809519/000095017022002348/gdrx-20211231.htm"
 
 xbrl_converter_api_endpoint = "https://api.sec-api.io/xbrl-to-json"
