4c7a16dd321c1dd97a9aaf912e8eca137862d4f0
tifffnguyen <105865921+tifffnguyen@users.noreply.github.com>
2022-06-29 05:44:17 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,4 +7,14 @@ import requests
 import matplotlib.pyplot as plit
 import json
 
+api_key = open('9bac03f9f1d1dc4b5cf06c47b0550cc36a71afe54e43330c8bc7f7802c126d70','r').read()
+
+company = "FB"
+years = 2
+
+income_statement = requests.get(f"https://financialmodelingprep.com/api/v3/income-statement/{company}?limit={years}apikey={api_key}")
+
+income_statement = income_statement.json()
+
+print(income_statement)
 
